17464978a17166d3eafdca1797f7ff66fdabd04c
Ismael G Marin C <ismael.marin@gmail.com>
2013-07-21 06:38:58 -0500

    Invitacion

--- app/mailers/invitacion.rb
+++ app/mailers/invitacion.rb
@@ -1,14 +1,13 @@
 class Invitacion < ActionMailer::Base
-  default from: "from@example.com"
+  default from: "hola@vueltadehoja.com"
 
   # Subject can be set in your I18n file at config/locales/en.yml
   # with the following lookup:
   #
   #   en.invitacion.cita.subject
   #
-  def cita
-    @greeting = "Hi"
-
-    mail to: "to@example.org"
+  def cita(invitado, direccion)
+    @direccion = direccion
+    mail to: invitado, subject: "Alguien esta para ayudarte"
   end
 end
